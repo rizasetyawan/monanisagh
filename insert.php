41297fb9785d550fe853f2a36ff8da67fe85f8af
annisahafenty <annisahafenty@gmail.com>
2018-12-04 23:58:36 +0700

    created by admin

--- insert.php
+++ insert.php
@@ -1,4 +1,6 @@
 <?php
+  session_start();
+  
   include "connection.php";
   if (isset($_POST["submit"])) {
     // $date = $_POST['date'];
@@ -11,9 +13,11 @@
     $qty = $_POST['qty'];
     $productcode = $_POST['productcode'];
     $submit = $_POST['submit'];
+    $admin = $_SESSION['username'];
 
 
-    $insert = "INSERT INTO stock SET id = 'NULL', created_date = '$date', season = '$season', product = '$product', color = '$color', status = '$status', qty = '$qty', created_by = 'riza', product_code = '$productcode'";
+
+    $insert = "INSERT INTO stock SET id = 'NULL', created_date = '$date', season = '$season', product = '$product', color = '$color', status = '$status', qty = '$qty', created_by = '$admin', product_code = '$productcode'";
     $result = mysqli_query($conn, $insert);
 
     if ($result) {
